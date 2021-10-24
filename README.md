672004b27a9d114e2bd5fa215c957152c6151533
Corben78 <corben.dallas@gmx.at>
2021-10-24 12:04:21 +0200

    Initial commit of install script
    +install.sh
    
    +README.md

---
+++ README.md
@@ -0,0 +1,100 @@
+# Streamer.Bot Linux Install Script
+
+With this script you can easily install the latest version of [Streamer.bot](https://github.com/Streamerbot/Streamer.bot) on Linux.
+It will create a command `streamer.dot` as well as a menu entry via `streamerbot.desktop`
+
+currently tested distributions:
+* Debian/Ubuntu based
+* Arch based
+
+# Usage
+
+* Grab the install.sh script, either directly or via git clone.
+* execute `./install.sh`
+
+# Default Settings
+
+## Paths
+* `streamer.bot` will be installed below `~/.local/bin`
+* uses `~/.local/lib/streamer.bot` for the programm files
+* uses `~/.local/lib/streamer.bot/pfx` for the wine prefix
+
+Make sure `.local/bin` is in your $PATH, e.g. add .local/bin to your $PATH via `.profile` or `.bash_profile` file:
+```bash
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+```
+This script will try to add these lines if possible.
+
+## Prerequisites
+
+Linux (doh!), with a standard desktop environment, bash, awk, wget, unzip
+
+optional: `jq`
+
+If the release will be optained automatically from github `jq` is needed, which will be attempted to be installed automatically.
+If an url or a local file of the release file is provided (see below), installation of `jq` can be skipped.
+
+mandatory: `wine`
+
+If not found, `wine` will be installed automatically from the repositories, maybe you want to install it manually before, as Ubuntu, Debian, Fedora will probably have outdated versions.
+
+* Ubuntu: https://wiki.winehq.org/Ubuntu (winehq-staging package recommended)
+* Debian: https://wiki.winehq.org/Debian
+* Fedora: https://wiki.winehq.org/Fedora
+
+Latest versions in rolling releases available:
+* Arch: pacman -S wine
+* openSuSE: zypper in wine
+
+mandatory: winetricks
+https://wiki.winehq.org/Winetricks
+https://github.com/Winetricks/winetricks
+
+* Arch: pacman -S winetricks
+* openSuSE: zypper in winetricks
+
+# Update
+
+```bash
+UPDATE=1 ./install.sh
+```
+
+If no other options are given, the script will grab the latest version from github and overwrite the existing files. Can be combined with specifying file location or URL (see below).
+
+# Use local file or specific URL
+
+If you downloaded Streamer.bot already (e.g. a beta), you can specify the location:
+```bash
+FILE=$HOME/Downloads/Streamer.bot-0.1.3-preview4.zip ./install.sh
+```
+
+Alternatively you can also provide a specifc url
+```bash
+URL=https://cdn.discordapp.com/attachments/879546641051422750/881631757550632970/Streamer.bot-0.1.3-preview4.zip ./install.sh
+```
+
+# Manuall install
+
+If you would like to manually install Streamer.bot:
+* create a fresh wine prefix, 32-bit (via WINEARCH=32) or 64-bit (default)
+* via winetricks install dotnet472 dxvk and d3dcompiler_47 into that prefix
+* run Streamer.bot with `WINEPREFIX=<prefix> wine Streamer.bot.exe >/dev/null 2>&1` within the path of the Streamer.bot.exe
+
+# Uninstall
+
+```bash
+UNINSTALL=1 ./install.sh
+```
+
+# Troubleshooting
+
+winetricks takes a while to complete (several minutes). especially if it has to download from web.archive.org.
+winetricks might get stuck, if you interrupt it with ctrl+c the script will try to end all remaining wine processes.
+Try to uninstall (see above) and install from scratch again.
+
+# Notes
+
+Big thanks to [nate1280](https://github.com/nate1280/) who rentlessly works on Streamer.bot and adjusted it to work with wine.
---
+++ install.sh
@@ -0,0 +1,200 @@
+#!/bin/bash
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# (c) 2021 FeVR     https://github.com/teamfevr
+#          corben78 https://github.com/corben78
+
+function check_distro () {
+    # if check has been done before, return immediately
+    [ ! -z "$PCKCMD" ] && return 0
+    # check distro, arch or debian based
+    if [ -f "/etc/os-release" ]; then
+        DISTRO=$(grep ^ID_LIKE= /etc/os-release | awk -F '=' '{ print $2 }')
+        if [ -z "$DISTRO" ]; then
+            DISTRO=$(grep ^ID= /etc/os-release | awk -F '=' '{ print $2 }')
+        fi
+    fi
+
+    case "$DISTRO" in
+        arch)
+            PCKCMD="pacman -S"
+            ;;
+        debian)
+            PCKCMD="apt install"
+            ;;
+        *)
+            unset DISTRO
+            ;;
+    esac
+
+    if [ -z "$DISTRO" ]; then
+        echo unsupported or unknown distribution, exiting
+        exit 2
+    fi
+}
+
+function is_installed () {
+    CMD=$1
+    if [ -z $(which "$CMD") ]; then
+        check_distro
+        echo $CMD not found, trying to install
+        sudo $PCKCMD $CMD
+        if [ "$?" -ne "0" ]; then
+            echo failed to install "$CMD". quitting.
+            exit 3
+        fi
+    fi
+}
+
+function rmtemp () {
+    if [ ! -z "$TMPDIR" ]; then
+        rm -r $TMPDIR
+    fi
+}
+
+LOCAL="$HOME/.local"
+SBBIN=$LOCAL/bin
+SBDESKTOPPATH="$HOME/.local/share/applications"
+
+# determine installation path
+if [ -z $(echo $PATH | grep $HOME/.local/bin) ]; then
+    check_distro
+    case "$DISTRO" in
+    arch)
+        if [ -f $HOME/.bash_profile ]; then
+            PROFILE=$HOME/.bash_profile
+        elif [ -f $HOME/.profile ]; then
+            PROFILE=$HOME/.profile
+        fi
+
+        if [ ! -z $PROFILE ] && [ -z $(grep .local/bin $PROFILE) ]; then
+            cat << EOF > $PROFILE
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+EOF
+        else
+            echo cannot determine how to add $HOME/.local/bin to $PATH
+            exit 9
+        fi
+        ;;
+    *)
+        unset $DISTRO
+        ;;
+    esac
+fi
+
+echo install path: $SBBIN
+
+SBPATH=$LOCAL/lib/streamer.bot
+SBPFX=$LOCAL/lib/streamer.bot/pfx
+
+if [ ! -z $UNINSTALL ]; then
+    [ -d $SBPATH ] && rm -r $SBPATH
+    [ -f $SBBIN/streamer.bot ] && $ rm $SBBIN/streamer.bot
+    [ -f $SBDESKTOPPATH/streamerbot.desktop ] && rm $SBDESKTOPPATH/streamerbot.desktop
+    echo Streamer.bot removed
+    exit 0
+fi
+
+if [ -d $SBPATH ] && [ -z $UPDATE ]; then
+    echo $SBPATH already exists. quitting.
+    exit 5
+fi
+
+# if not file is given, and no url, grab latest version from github
+if [ -z $FILE ]; then
+    if [ -z $URL]; then
+        is_installed jq && echo jq is installed
+        URL=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/Streamerbot/Streamer.bot/releases | jq -r ".[0].assets[].browser_download_url")
+    fi
+
+    is_installed wget && echo wget is installed
+    TMPDIR=$(mktemp -d)
+    cd $TMPDIR
+    wget $URL
+    if [ "$?" -ne "0" ]; then
+        echo download from url $URL failed. exiting.
+        exit 4
+    fi
+    FILE="$TMPDIR"/"$(basename $URL)"
+else
+    # change to absolute path, just in case a relative path was given
+    FILE=$(readlink -f $FILE)
+fi
+
+mkdir -p $SBPATH
+if [ "$?" -ne "0" ]; then
+    echo cannot create $SBPATH. quitting.
+    exit 6
+fi
+
+cd $SBPATH
+unzip -o $FILE
+if [ "$?" -ne "0" ]; then
+    echo problem extracting $FILE. quitting.
+    rmtemp
+    exit 7
+fi
+
+rmtemp
+
+if [ ! -z $UPDATE ]; then
+    echo update finished
+    exit 0
+fi
+
+# pre-requisites
+is_installed wine && echo wine is installed
+is_installed winetricks && echo winetricks is installed
+
+WINEPREFIX=$SBPFX wineboot
+WINEPREFIX=$SBPFX winetricks -q dotnet472 dxvk d3dcompiler_47
+
+# if interrupted by ctrl+c
+if [ "$?" -eq "130" ]; then
+    echo winetricks failed. killing remaining processes.
+    ps -A | grep -i -e wine -e .exe | awk '{ print $1 }' | sort -r | xargs kill -TERM
+    ps -A | grep -i -e wine -e .exe | awk '{ print $1 }' | sort -r | xargs kill -KILL
+    exit 8
+fi
+
+# create command
+cat << EOF > $SBBIN/streamer.bot
+#!/bin/bash
+cd $SBPATH
+DISABLE_MANGOHUD=1 WINEPREFIX=$SBPFX wine Streamer.bot.exe >/dev/null 2>&1
+EOF
+chmod +x $SBBIN/streamer.bot
+
+#create desktop file
+cat << EOF > $SBDESKTOPPATH/streamerbot.desktop
+[Desktop Entry]
+Name=Streamer.bot
+StartupWMClass=streamer.bot.exe
+Comment=Bot for Twitch Streamers
+GenericName=Chatbot
+Exec=env DISABLE_MANGOHUD=1 WINEPREFIX=$SBPFX wine Streamer.bot.exe
+Icon=$SBPATH/streamer.bot.png
+Type=Application
+Categories=Network
+Path=$SBPATH
+EOF
+
+echo installation successful.
+
+exit 0
